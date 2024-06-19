1d15bb6d166e256d12db74998a36bab55746650a
Benny Jasper Kummari <168684919+bennyjasp09@users.noreply.github.com>
2024-06-19 03:38:19 -0400

    Delete dist directory

--- dist/routes/f1Routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const f1Controller_1 = require("../controllers/f1Controller");
-const router = (0, express_1.Router)();
-router.get('/', f1Controller_1.getAllResults);
-exports.default = router;
