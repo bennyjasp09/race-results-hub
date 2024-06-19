fbc388822634e0a4e91b2d0ea125b9c0df666ffa
Benny Jasper <bennyjasper46@gmail.com>
2024-06-19 04:20:41 -0400

    Fix directory structure and remove unwanted files

--- src/routes/f1Routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getAllResults } from '../controllers/f1Controller';
-
-const router = Router();
-
-router.get('/', getAllResults);
-
-export default router;
